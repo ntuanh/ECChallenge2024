dd254c907ce4b74686163f24d8cab7bd99ea5e0a
ntuanh <ntuanh2705@gmail.com>
2024-09-28 19:39:51 +0700

    [UPDATE] update version 3,0 Nhiem vu 3

--- NHIEM VU 3/Main.cpp
+++ NHIEM VU 3/Main.cpp
@@ -1,53 +1,51 @@
+#include<Leanbot.h>
 
-#include <Leanbot.h>                    // sử dụng thư viện Leanbot
+const int time_delay = 1000 ;
+const int threshold =  50 ;
 
-const int time_delay = 100;
-const int threshold = 50;
-
-void setup() {
-    Leanbot.begin();
-    Serial.begin(9600);
+void setup(){
+  Leanbot.begin();
+  Serial.begin(9600);
 }
 /***************************************************************************************************************
-The common part
+The common part 
 -> TurnRight and Turn Left  ( int speed , int angle)
+-> Forward 
 -> FollowLine ( int speed ) ( 2 option )
--> Grip ( int unit_angle ) ( 1 option)
+-> Whitespace
+-> Grip ( int time_angle ) ( 1 option)
+-> GripBreak ( int time_angle ) 
+-> Junction 
 ***************************************************************************************************************/
-void TurnRight(int speed, int angle)
+void TurnRight( int speed , int angle )
 {
-    LbMotion.runLR(speep, -1 * speed);
-    LbMotion.waitRotationDeg(angle);
-    LbMotion.stopAndWait();
-    LbDelay(time_delay);
+  LbMotion.runLR(speed , -1 * speed );                     // let Leanbot rotate right
+  LbMotion.waitRotationDeg(angle);  
+  LbMotion.stopAndWait() ;
+  LbDelay(time_delay);
 }
 
-void TurnLeft(int speed, int angle)
+void TurnLeft( int speed , int angle )
 {
-    LbMotion.runLR(-1 * speep, speed);
-    LbMotion.waitRotationDeg(angle);
-    LbMotion.stopAndWait();
-    LbDelay(time_delay);
+  LbMotion.runLR(-1 * speed , speed );                     // let Leanbot rotate right
+  LbMotion.waitRotationDeg(angle);  
+  LbMotion.stopAndWait() ;
+  LbDelay(time_delay);
 }
 
-void Forward(int speed, int distance)
+void Forward ( int speed , int distance )                    // 
 {
-    LbMotion.runLR(speed, speed);
-    LbMotion.waitDistanceMm(distance);
-    LbMotion.stopAndWait();
-    LbDelay(time_delay);
-}
-void Forward1(int speed)
-{
-    LbMotion.runLR(speed, speed);
-    LbDelay(time_delay);
+  LbMotion.runLR( speed , speed );                     // let Leanbot rotate right
+  LbMotion.waitDistanceMm(distance)  ;
+  LbMotion.stopAndWait() ;
+  //LbDelay(time_delay);
 }
 // Follow Line : Solution 1  
 /*
-void FollowLine( int speed )
-{
+void FollowLine( int speed ) 
+{         
     int vL = speed ;
-    int vR = speed ;
+    int vR = speed ;              
     byte line = LbIRLine.read(threshold);
     switch (line) {
     case 0b0100:
@@ -72,129 +70,182 @@ void FollowLine( int speed )
 }
 */
 // Follow Line : Solution 2 
-void FollowLine(int speed)
+void FollowLine ( int speed  )
 {
-    // div level speed 
-    int speed_0 = 0;
-    int speed_1 = 0.2 * speed;
-    int speed_2 = 0.4 * speed;
-    int speed_3 = 0.6 * speed;
-    int speed_4 = 0.8 * speed;
+  // div level speed 
+  int speed_0 = 0  ;
+  //int speed_1 = 0.2 * speed ;
+  int speed_2 = 0.4 * speed ;
+  int speed_3 = 0.6 * speed ;
+  //int speed_4 = 0.8 * speed ;
 
-    byte line = LbIRLine.read(threshold);                   // Read the value of 4 bar sensors with a threshold of 50
-    LbIRLine.println(line);                                 // transfer the results to the computer
+  byte line = LbIRLine.read(threshold);                   // Read the value of 4 bar sensors with a threshold of 50
+  LbIRLine.println(line);                                 // transfer the results to the computer
 
-    switch (line) {                                         // check the location of Leanbot
+  switch (line) {                                         // check the location of Leanbot
     case 0b0000:
     case 0b0110:
-        LbMotion.runLR(speed, speed);
-        break;
+      LbMotion.runLR(speed, speed);
+      break;
+
 
     case 0b0010:
-        LbMotion.runLR(speed, speed_3);
-        break;
+      LbMotion.runLR(speed, speed_3);
+      break;
 
     case 0b0011:
-        LbMotion.runLR(speed, speed_2);
-        break;
+      LbMotion.runLR(speed, speed_2);
+      break;
 
     case 0b0001:
-        LbMotion.runLR(speed, speed_0);
-        break;
+      LbMotion.runLR(speed, speed_0);
+      break;
 
     case 0b0100:
-        LbMotion.runLR(speed_3, speed);
-        break;
+      LbMotion.runLR(speed_3, speed);
+      break;
 
     case 0b1100:
-        LbMotion.runLR(speed_2, speed);
-        break;
+      LbMotion.runLR(speed_2, speed);
+      break;
 
     case 0b1000:
-        LbMotion.runLR(speed_0, speed);
-        break;
+      LbMotion.runLR(speed_0, speed);
+      break;
+
 
     case 0b1111:
-        LbMotion.runLR(speed_0, speed_0);
-        break;
-    }
+      LbMotion.runLR(speed_0, speed_0);
+      break;
+  }
+}
+
+//Check white space 
+bool whiteSpace()
+{
+  byte line = LbIRLine.read(threshold); 
+  if ( line == 0b0000 )return true ;else return false ;
 }
 
 // Grip :
-void Grip(int time_angle)
+void Grip( int time_angle )
 {
-    for (int i = 0; i <= 90; i += 10) {
-        LbGripper.moveToLR(i, i, time_angle);
-    }
-    LbMotion.stopAndWait();
-    LbDelay(time_delay);
+  for ( int i = 0 ; i <= 90  ; i += 10 )
+    LbGripper.moveToLR(i, i , time_angle);
+  LbMotion.stopAndWait() ;
+  LbDelay(time_delay);
 }
 
-void EscapeHose() {                 // Push the escape hose to the rescue point
-    TurnRight(500, 180);
-    while (isBlackDetected())
-    {
-        Forward1(500);
-    }
-    LbMotion.stopAndWait();
-    LbDelay(time_delay);
-    TurnRight(500, 180);
+// Grip break 
+void GripBreak( int time_angle)
+{
+  for (int i = 90; i >= 0; i -= 10) 
+  LbGripper.moveToLR( i , i , time_angle);
 }
 
-void CheckHuman()                       // Check human and pick up 
+bool junction()                                     // check junction road 
 {
-    FollowLine();
-    Foward(500, 20);
-    TurnRight(500, 25);
-    int distance = Leanbot.pingMm();
-    if (distance < 200)               // Occupied
-    {
-        TurnLeft(500, 25);
-        FollowLine();
-        Forward(500, 45);
-        TurnRight(500, 90);
-        Forward(500, 50);
-        Grip(500);
-        TurnRight(500, 180);
-        FollowLine();
-        Forward(500, 45);
-        TurnLeft(500, 90);
-    }
-    else if (distance > 200)           // Unoccupied
-    {
-        TurnLeft(500, 25);
-        FollowLine();
-        Forward(500, 45);
-        TurnLeft(500, 90);
-        Forward(500, 50);
-        Grip(500);
-        TurnRight(500, 180);
-        FollowLine();
-        Forward(500, 45);
-        TurnRight(500, 90);
-    }
+  byte line = LbIRLine.read(threshold); 
+  if ( line == 0b1111 )return true ;
+  if ( line == 0b1110 )return true ;
+  if ( line == 0b0111 )return true ;
+  return false ;
 }
 
-void HumanToRescuePoint()
+/***************************************************************************************************************
+the first challenge ( NHIEMVU2 )
+**************************************************************************************************************/
+
+class NhiemVu3{
+  private :
+    int speedrun  = 500 ;                   // mm 
+    int speedfl = 500 ;                     // mm 
+    int speedturn = 500 ;
+    int limit_angle =  60  ;
+    int time_angle = 500 ;
+
+    int distance_back = 50 ;
+    unsigned int distance_human = 60 ;
+
+    int distance_add1 = 15 ;
+    int distance_add2 = 30 ;
+
+  public :
+    void Processing();
+};
+
+void NhiemVu3::Processing() 
 {
-    while (isBlackDetected())
-    {
-        Forward1();
+  // Mode 1  
+  while (LbTouch.read(TB2A) == 0)LbDelay(10);
+  while ( !whiteSpace() ){
+    if(!junction() )Forward(speedrun , 5);
+    else {FollowLine(speedfl) ;}
+  }
+  LbMotion.stopAndWait();
+
+  // Mode 2 
+  while (LbTouch.read(TB2A) == 0)LbDelay(10);
+  Forward(-1 * speedrun , distance_back);
+  TurnLeft( speedturn , 180 );
+  while ( !junction() ){
+    FollowLine(speedfl);
+  }
+
+  bool check_human_right = false ;
+  // Mode 3 : turn right 
+  int angle = 0 ;
+  while ( angle < limit_angle ){
+    TurnRight(speedturn , 5 );
+    angle += 5 ;
+    if ( Leanbot.pingMm() <= distance_human){
+      check_human_right = true ;
+      TurnLeft(speedturn , angle) ;
+      break ;
     }
-    LbMotion.stopAndWait();
-    LbDelay(time_delay);
-    LbGripper.open();
-    LbDelay(time_delay);
-    TurnRight(500, 180);
+  }
+
+  // Mode 4 : safe :
+  while (LbTouch.read(TB2A) == 0)LbDelay(10);
+  while(!junction())FollowLine(speedfl) ;
+  Forward(speedrun , distance_add1);
+  if ( check_human_right){
+    TurnRight(speedturn, 90);
+  }else {
+    TurnLeft(speedturn , 90 );
+  }
+  Forward(speedrun , distance_add2);
+
+  // Mode 5 : Grip 
+  while (LbTouch.read(TB2A) == 0)LbDelay(10);
+  Grip(time_angle) ;
+  TurnLeft(speedturn , 180) ;
+  while (!junction())FollowLine(speedfl);
+
+  //Mode 6 : 
+  while (LbTouch.read(TB2A) == 0)LbDelay(10);
+  if ( check_human_right ){
+    TurnLeft(speedrun , 90);
+  }else TurnRight( speedrun , 90);
+
+  //Mode 7 :
+  while (LbTouch.read(TB2A) == 0)LbDelay(10);
+  while(!whiteSpace()){
+    if(!junction())FollowLine(speedfl);else Forward(speedrun , 5);
+  }
+
+  // Mode 8: break grip 
+  while (LbTouch.read(TB2A) == 0)LbDelay(10);
+  GripBreak( time_angle) ;
+  while(!junction() )FollowLine(speedfl);
+  TurnRight( speedturn , 90);
+
+  // Mode 9 : go to safe 
+  while (LbTouch.read(TB2A) == 0)LbDelay(10);
+  while ( !whiteSpace() )FollowLine(speedfl);
 }
 
-void Safe()
-{
-    FollowLine();
-    Forward(500, 45);
-    TurnRight();
-    FollowLine();
-    TurnLeft();
-    Forward(500, 60);
-    TurnLeft(500, 90);
+void loop(){
+  NhiemVu3 nv3 ;
+  nv3.Processing();
 }
